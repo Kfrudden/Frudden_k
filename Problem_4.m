bb4bb88fd9494213e36f5bcdeaf009342d6808d7
Kfrudden <94567517+Kfrudden@users.noreply.github.com>
2021-11-24 12:35:47 -0800

    Add files via upload

--- Problem_4.m
+++ Problem_4.m
@@ -4,10 +4,22 @@
 % problem 4: ployvals
 clc, clear, format compact
 
-x=input('x= ');
-y=input('y= ');
+%%
+x= 200
+y= 759
 f_xy(x,y)
 
+
+%%
+clc, clear 
+
+x=[1:10]'
+y=[1:10]'
+
+f_xy(2,3);
+
+fprintf('\nThe value of g(2,3) is %4.6f\n',f_xy(2,3))
+
 function [g]= f_xy(x,y)
-    g=(x.^3+x.*y+(x-3).^2-y^(-0.2))/1000;
+    g=(x.^3+x.*y+(x-3).^2-y.^(-0.2))/1000;
 end
